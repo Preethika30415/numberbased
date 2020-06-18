3b97422137e1da3c8a430ea3a620d490c6ddfe50
srinivas <srinivas@sushma>
2020-06-18 13:01:17 +0530

    sum of digits in a given number

---
+++ Numberproblems/src/com/practice/numberbased/N5.java
@@ -0,0 +1,17 @@
+package com.practice.numberbased;
+
+public class N5 
+{
+	public static void main(String[] args) 
+	{
+		int n=Integer.parseInt(args[0]);
+		int c=0;
+		while(n!=0) 
+		{
+			int rem=n%10;
+			n=n/10;
+			c+=rem;
+		}
+		System.out.println("The sum of digits is "+c);
+	}
+}
