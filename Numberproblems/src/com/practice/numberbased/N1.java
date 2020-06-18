cc61978253f274148bd6b4995e6bc7b8fe78b0f2
srinivas <srinivas@sushma>
2020-06-18 12:00:34 +0530

    sum of 1st n natural numbers

---
+++ Numberproblems/src/com/practice/numberbased/N1.java
@@ -0,0 +1,17 @@
+package com.practice.numberbased;
+
+public class N1 
+{
+	public static void main(String[] args) 
+	{
+		int n= Integer.parseInt(args[0]);
+		int sum=0,sum1=0;
+		for(int i=1;i<=n;i++) 
+		{
+			sum+=i;
+		}
+		sum1=(n*(n+1))/2;
+		System.out.println("Sum by using loops is "+sum);
+		System.out.println("Sum by using formula is "+sum1);
+	}
+}
